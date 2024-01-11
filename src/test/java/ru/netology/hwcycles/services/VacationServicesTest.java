41245a66924dcc300d0c52b2445186c723a17a9e
AnF <anfwork007@gmail.com>
2024-01-11 23:52:26 +0300

    simple tests added

--- src/test/java/ru/netology/hwcycles/services/VacationServicesTest.java
+++ src/test/java/ru/netology/hwcycles/services/VacationServicesTest.java
@@ -1,11 +1,31 @@
 package ru.netology.hwcycles.services;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServicesTest {
+
+    @Test
+    public void testVacationConditionSmallSalary () {
+        VacationServices services = new VacationServices();
+        int expected = 3;
+        int actual = services.calculate(10000, 3000, 20000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testVacationConditionBigSalary () {
+        VacationServices services = new VacationServices();
+        int expected = 2;
+        int actual = services.calculate(100000, 60000, 150000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 
     @ParameterizedTest
     @CsvSource({
